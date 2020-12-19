732d7aa1088e194039fe4d79934ea00f756827df
souravpal16 <souravpal.4916@gmail.com>
2020-12-19 09:12:38 +0530

    single items and urls added

--- backend/items/views.py
+++ backend/items/views.py
@@ -28,11 +28,35 @@ def all_items(request):
         item_serializer = ItemSerializer(data=item_data)
         if item_serializer.is_valid():
             item_serializer.save()
-            added_item = Item.objects.get(product_id = item_data['product_id'])
-            add_to_update_sheet(added_item, added_item)
             return JsonResponse(item_serializer.data, status=status.HTTP_201_CREATED) 
         return JsonResponse(item_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         count = Item.objects.all().delete()
         return JsonResponse({'message': '{} Deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def single_item(request, product_id):
+
+    try: 
+        item = Item.objects.get(product_id = product_id) 
+    except Item.DoesNotExist: 
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+ 
+    if request.method == 'GET': 
+        item_serializer = ItemSerializer(item) 
+        return JsonResponse(item_serializer.data) 
+ 
+    elif request.method == 'PUT':
+
+        item_data = JSONParser().parse(request) 
+        item_serializer = ItemSerializer(item, data=item_data) 
+        if item_serializer.is_valid(): 
+            item_serializer.save()
+            return JsonResponse(item_serializer.data) 
+        return JsonResponse(item_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+ 
+    elif request.method == 'DELETE': 
+        item.delete() 
+        return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
